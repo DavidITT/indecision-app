930a2b329703412c4f32208e91685aba562273c9
David Villeda <42588903+DavidITT@users.noreply.github.com>
2024-12-17 16:46:51 -0600

    feat: Sticky navbar

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex items-center justify-between p-4 bg-card text-card-foreground border-b border-border bg-white">
-    <div class="inline-flex gap-3 items-center">
-      <img class="w-10 h-10 rounded-full border border-blue-300" src="https://gravatar.com/avatar/9a89865235002c16212eb8e47989e5dd?s=400&d=robohash&r=x" alt="Profile img">
+  <div class="sticky top-0 z-10">
+    <div class="flex items-center justify-between p-4 bg-card text-card-foreground border-b border-border bg-white">
+      <div class="inline-flex gap-3 items-center">
+        <img class="w-10 h-10 rounded-full border border-blue-300" src="https://gravatar.com/avatar/9a89865235002c16212eb8e47989e5dd?s=400&d=robohash&r=x" alt="Profile img">
+      </div>
+      <ChatMenu/>
     </div>
-   <ChatMenu/>
+    <SearchBar v-if="searchbar"/>
   </div>
-  <SearchBar v-if="searchbar"/>
 </template>
 
 <script setup lang="ts">
